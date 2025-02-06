4aebc53b53f5dadbac96626ffbeefd47b8d79cf7
Lazar Ilic <lilic141991@gmail.com>
2025-02-06 11:28:58 +0100

    link lib project package with test-app

---
+++ test-app/src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './App.css'
+import { MaskedInput } from "@energma/input-mask-react"
+
+
+function App() {
+
+  const [value, setValue] = useState('');
+
+   interface Schema {
+    mask: string;
+    symbol: string;
+    type?: 'numbers' | 'letters' | 'mixed';
+}
+
+  const schema: Schema = {
+      mask: '___/___-___-___',
+      symbol: '_', 
+      type: 'numbers',  
+     };
+
+  return (
+    <>
+       <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Masked Input Example</h1>
+      <p>Enter a value in the input field below:</p>
+      <MaskedInput
+        schema={schema}
+        value={value}
+        onChange={(e: any) => setValue(e.target.value)}
+        placeholder="Enter value..."
+        data-testid="masked-input"
+        className='masked-input'
+      />
+      <p>Current Value: {value}</p> 
+      </div>
+    </>
+  )
+}
+
+export default App
