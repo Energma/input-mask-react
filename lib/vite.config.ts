14c5cfa22a48f57f54c0c39b310c445d61cce6fd
lilic14991 <lilic14991@gmail.com>
2025-03-13 15:34:42 +0100

    remove devDep enable to externalize dependencies from parent

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import path from "path";
-
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,24 +10,25 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
       outDir: "dist",
-    })
+    }),
   ],
   build: {
     outDir: "dist",
     lib: {
       entry: path.resolve(__dirname, "./index.tsx"),
-      name: "@energma/input-mask-react", 
-      formats: ["es", "umd"], 
+      name: "@energma/input-mask-react",
+      formats: ["es", "umd"],
       fileName: (format) => `input-mask-react.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
-        }
-      }
-    }
-  }
-})
+          "react/jsx-runtime": "jsxRuntime",
+        },
+      },
+    },
+  },
+});
